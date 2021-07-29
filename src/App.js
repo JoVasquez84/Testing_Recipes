3e77fac3e3d99f9f2f865db69338c42029b9ed23
student <studemt@students-MacBook-Pro.local>
2021-07-29 14:57:38 -0700

    Added Recipe code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isAddRecipeFormDisplayed: false,
+    recipes: [],
+    newRecipeName: '',
+    newRecipeInstructions: ''
+  }
+  
+
+  toggleAddRecipeForm = () => {
+    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+    const target = event.target;
+    const name = target.name;
+  
+    this.setState({[name]: target.value});
+  }
+
+  submitRecipe = (event) => {
+    event.preventDefault()
+    this.state.recipes.push({
+          name: this.state.newRecipeName,
+          instructions :this.state.newRecipeInstructions
+    })
+  }
+    
+
+  render(){
+    const addNewRecipeForm = (
+      <form id="recipe-form" onSubmit={this.submitRecipe}>
+        <label htmlFor="newRecipeName">Recipe name: </label>
+        <input type="text"
+          name="newRecipeName"
+          id="newRecipeName"
+          onChange={this.handleChange}
+          value={this.state.newRecipeName} />
+        <label htmlFor="newRecipeInstructions">Instructions:</label>
+        <textarea name="newRecipeInstructions"
+          id="newRecipeInstructions"
+          placeholder="write recipe instructions here..."
+          onChange={this.handleChange}
+          value={this.state.newRecipeInstructions} />
+        <input type="submit" />
+      </form>
+    )
+
+    return (
+      <div className="App">
+        <h1 className="App-header">My Recipes</h1>
+        {
+          this.state.isAddRecipeFormDisplayed
+          ? addNewRecipeForm
+          : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
+        }
+        {
+          this.state.recipes.length > 0 ?
+          <ul>
+            {this.state.recipes.map(item  => <li id="">{item.name}</li>)}
+            {console.log(this.state.recipes)}
+            {console.log(this.state.recipes[0])}
+            {console.log(this.state.recipes[1])}
+          </ul> :
+          <p>There are no recipes to list.</p>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
